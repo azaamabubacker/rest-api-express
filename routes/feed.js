5069e99652f4f0d344a3bd53fc6eb6c999a201a0
azaamabubacker <cleancodelk@gmail.com>
2023-04-05 14:22:35 +0530

    Add Post Route

--- routes/feed.js
+++ routes/feed.js
@@ -3,6 +3,9 @@ const feedController = require("../controller/feed");
 
 const router = express.Router();
 
+// GET /feed/posts
 router.get("/posts", feedController.getPosts);
+// POST /feed/post
+router.post("/post", feedController.createPost);
 
 module.exports = router;
